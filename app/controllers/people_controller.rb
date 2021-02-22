3977b52edd9c5921fcef735eec9bd5cc8bd3012c
romanolivi <romano_livi@outlook.com>
2021-02-22 13:22:13 -0500

    commit tree 17433b9a8d8da6b0683956b4f35d553cb7cd702c

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,8 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')   
   end
 
   def create    
@@ -15,6 +17,6 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(:name, addresses_attributes: [])
   end
 end
